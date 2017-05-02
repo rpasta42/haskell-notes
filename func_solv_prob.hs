0f7c52f3a23814b051c08c25951dae95f28734bd
Konstantin Kowalski <kostelkow@gmail.com>
2017-05-02 16:27:28 +0000

    RPN solver

--- func_solv_prob.hs
+++ func_solv_prob.hs
@@ -20,34 +20,33 @@ pushStack x xs = x:xs
 popStack :: [a] -> (a, [a])
 popStack (x:xs) = (x, xs)
 
+getRpnList = words --filter ((/=) " ") $ words --partition ((==) ' ') str -- group str
+isOp x = or $ map (==x) ["+", "-", "*"]
 
-getRpnList str = filter ((/=) " ") $ group str
-
-isOp x = or $ map (==x) ['+', '-', '*']
-
-getOp '+' = (+)
-getOp '-' = (-)
-getOp '*' = (*)
+getOp "+" = (+)
+getOp "-" = (-)
+getOp "*" = (*)
 
 strToInt s = (read s) :: Int
 
+--solveOp :: (Num a, Read a) => (a -> a -> a) -> String -> String -> a
 solveOp op a b =
-   let numA = strToInt a
-       numB = strToInt b
+   let numA = a-- (read a) --strToInt a
+       numB = b--(read b) --strToInt b
    in op numA numB
 
-solveRPN :: (Num a) => String -> a
+solveRPN :: (Read a, Num a) => String -> a
 solveRPN str = helper (getRpnList str) newStack
 
-
+helper :: (Read a, Num a) => [String] -> [a] -> a
 helper [] stack = fst . popStack $ stack
 helper (x:xs) stack
    | isOp x =
       let (operand1, stack1) = popStack stack
           (operand2, stack2) = popStack stack1
           op = getOp x
-      in helper xs (pushStack $ solveOp op operand1 operand2)
-   | otherwise = helper xs (pushStack $ strToInt x)
+      in helper xs $ pushStack (solveOp op operand1 operand2) stack2
+   | otherwise = helper xs (pushStack (read x) stack)
 
 input = "10 4 3 + 2 * -"
 
